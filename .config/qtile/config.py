fa6fbfe00b7df594570073e297f44e15021ca81f
Honi <honihoneyhowni@gmail.com>
2024-10-11 06:01:52 -0600

    made qtile more portable

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,20 +1,29 @@
 import os
 import subprocess
-from libqtile import bar, layout, widget, hook
+
+from libqtile import bar, hook, layout, widget
 from libqtile.config import Click, Drag, Group, Key, KeyChord, Match, Screen
 from libqtile.lazy import lazy
 
 ## Variables
 mod = "mod4"
-home = os.path.expanduser('~/')
+home = os.path.expanduser("~/")
 terminal = "kitty"
 browser = "firefox"
 idenv = "vscodium"
 config = home + "/.config/qtile/"
+platform = subprocess.run(
+    ["hostnamectl", "chassis"], stdout=subprocess.PIPE
+).stdout.decode("utf-8")[:-1]
+backlight = subprocess.run(
+    ["ls", "/sys/class/backlight/"], stdout=subprocess.PIPE
+).stdout.decode("utf-8")[:-1]
+
 
 ## Custom functions
 def txt_remove(text):
     return ""
+
 
 # Hide/Show all windows in a group
 @lazy.function
@@ -23,54 +32,87 @@ def minimize_all(qtile):
         if hasattr(win, "toggle_minimize"):
             win.toggle_minimize()
 
+
 ## Keys
 keys = [
     # Essentials
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod, "shift"], "Return", lazy.spawn("rofi -matching fuzzy -show drun"), desc="Spawn rofi"),
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.spawn("rofi -matching fuzzy -show drun"),
+        desc="Spawn rofi",
+    ),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     Key([mod], "b", lazy.hide_show_bar(), desc="Toggle the bar"),
-    KeyChord([mod], "a", [
-        KeyChord([], "w", [
-            Key([], "w", lazy.spawn(browser), desc="Launch browser"),
-            Key([], "e", lazy.spawn("firefox -P Escuela"), desc="Profile for school"),
-        ], name="Browser"),
-        Key([], "c", lazy.spawn(idenv), desc="Launch VSCode"),
-    ], name="Apps"),
+    KeyChord(
+        [mod],
+        "a",
+        [
+            KeyChord(
+                [],
+                "w",
+                [
+                    Key([], "w", lazy.spawn(browser), desc="Launch browser"),
+                    Key(
+                        [],
+                        "e",
+                        lazy.spawn("firefox -P Escuela"),
+                        desc="Profile for school",
+                    ),
+                ],
+                name="Browser",
+            ),
+            Key([], "c", lazy.spawn(idenv), desc="Launch VSCode"),
+        ],
+        name="Apps",
+    ),
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "c", lazy.spawn(f"kitty nvim {config}"), desc="Edit config"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"], "space", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
-
+    Key(
+        [mod, "shift"],
+        "space",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h",
+    Key(
+        [mod, "control"],
+        "h",
         lazy.layout.grow_left().when(layout=["bsp", "columns"]),
         lazy.layout.grow().when(layout=["monadtall", "monadwide"]),
         desc="Grow window to the left",
     ),
-    Key([mod, "control"], "l",
+    Key(
+        [mod, "control"],
+        "l",
         lazy.layout.grow_right().when(layout=["bsp", "columns"]),
         lazy.layout.shrink().when(layout=["monadtall", "monadwide"]),
         desc="Grow window to the right",
@@ -79,21 +121,38 @@ keys = [
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     Key([mod], "m", lazy.layout.maximize(), desc="Toggle between min and max sizes"),
-    Key([mod], "t", lazy.window.toggle_floating(), desc='Toggle floating'),
-    Key([mod], "f", lazy.window.toggle_fullscreen(), desc='Toggle fullscreen'),
-    Key([mod, "shift"], "m", minimize_all(), desc="Toggle hide/show all windows on current group"),
-
-    # Dmenu/Rofi
-    KeyChord([mod], "p", [
-        Key([], "e", lazy.spawn("rofi -show emoji -emoji-mode menu"), desc="Emoji selector"),
-        Key([], "n", lazy.spawn("rofi -show nerdy"), desc="Nerd icon selector"),
-        Key([], "i", lazy.spawn("wifimenu"), desc="Launch Rofi Wifi Menu"),
-        Key([], "y", lazy.spawn("yt-watch"), desc="Play youtube video in terminal")],
-        name="Run script"
+    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating"),
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
+    Key(
+        [mod, "shift"],
+        "m",
+        minimize_all(),
+        desc="Toggle hide/show all windows on current group",
     ),
-
+    # Dmenu/Rofi
+    KeyChord(
+        [mod],
+        "p",
+        [
+            Key(
+                [],
+                "e",
+                lazy.spawn("rofi -show emoji -emoji-mode menu"),
+                desc="Emoji selector",
+            ),
+            Key([], "n", lazy.spawn("rofi -show nerdy"), desc="Nerd icon selector"),
+            Key([], "i", lazy.spawn("wifimenu"), desc="Launch Rofi Wifi Menu"),
+            Key([], "y", lazy.spawn("yt-watch"), desc="Play youtube video in terminal"),
+        ],
+        name="Run script",
+    ),
     # Media keys
-    Key([], "XF86Calculator", lazy.spawn("rofi -show calc -no-show-match -no-sort"), desc="Launch Calculator"),
+    Key(
+        [],
+        "XF86Calculator",
+        lazy.spawn("rofi -show calc -no-show-match -no-sort"),
+        desc="Launch Calculator",
+    ),
     Key([], "XF86AudioMute", lazy.spawn("pamixer --toggle-mute")),
     Key([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 2")),
     Key([], "XF86AudioRaiseVolume", lazy.spawn("pamixer --increase 2")),
@@ -106,18 +165,40 @@ keys = [
 ]
 
 groups = []
-group_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",]
+group_names = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+]
 
-group_labels = ["󰅩", "", "", "", "󰺵", "󰭹", "", "", "", "󰑴",]
-#group_labels = ["DEV", "WWW", "SYS", "DOC", "GAMR", "CHAT", "MUS", "VID", "GFX",]
+group_labels = [
+    "󰅩",
+    "",
+    "",
+    "",
+    "󰺵",
+    "󰭹",
+    "",
+    "",
+    "",
+    "󰑴",
+]
+# group_labels = ["DEV", "WWW", "SYS", "DOC", "GAMR", "CHAT", "MUS", "VID", "GFX",]
 
-#group_layouts = ["monadtall", "monadtall", "tile", "tile", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall"]
+# group_layouts = ["monadtall", "monadtall", "tile", "tile", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall"]
 
 for i in range(len(group_names)):
     groups.append(
         Group(
             name=group_names[i],
-            #layout=group_layouts[i].lower(),
+            # layout=group_layouts[i].lower(),
             label=group_labels[i],
         )
     )
@@ -143,68 +224,73 @@ for i in groups:
     )
 
 colors = []
-cache = home +'.cache/wal/colors'
+cache = home + ".cache/wal/colors"
+
+
 def load_colors(cache):
-    with open(cache, 'r') as file:
+    with open(cache, "r") as file:
         for i in range(16):
             colors.append(file.readline().strip())
-    colors.append('#ffffff')
+    colors.append("#ffffff")
     lazy.reload()
+
+
 load_colors(cache)
 
-layout_theme = {"border_width": 2,
-                "margin": 4,
-                "border_focus": colors[12],
-                "border_normal": colors[3]
-                }
+layout_theme = {
+    "border_width": 2,
+    "margin": 4,
+    "border_focus": colors[12],
+    "border_normal": colors[3],
+}
 
 layouts = [
-    layout.Columns(**layout_theme,
-                   border_focus_stack=colors[12],
-                   border_normal_stack=colors[3]),
+    layout.Columns(
+        **layout_theme, border_focus_stack=colors[12], border_normal_stack=colors[3]
+    ),
     layout.MonadTall(**layout_theme),
-    #layout.Tile(
+    # layout.Tile(
     #    shift_windows = True,
     #    border_width = 0,
     #    margin = 0,
     #    ratio = 0.335,
-    #),
+    # ),
     layout.Max(
-        border_width = 0,
-        margin = 0,
+        border_width=0,
+        margin=0,
     ),
 ]
 
 widget_defaults = dict(
-    font = "M PLUS 1",
-    fontsize = 12,
-    padding = 2,
-    background = colors[0],
+    font="M PLUS 1",
+    fontsize=12,
+    padding=2,
+    background=colors[0],
 )
 extension_defaults = widget_defaults.copy()
 
-bar_thickness=20
+bar_thickness = 20
 screens = [
     Screen(
         top=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(
-                    active = colors[9],
-                    inactive = colors[2],
+                    active=colors[9],
+                    inactive=colors[2],
                     highlight_method="line",
-                    highlight_color=[f"{colors[0]}",f"{colors[2]}"],
-                    font = "Symbols Nerd Font Mono",
+                    highlight_color=[f"{colors[0]}", f"{colors[2]}"],
+                    font="Symbols Nerd Font Mono",
                 ),
                 widget.Prompt(),
                 widget.WindowName(),
-                #widget.TaskList(
+                # widget.TaskList(
                 #    parse_text=txt_remove,
                 #    background = colors[9],
                 #    txt_floating="🗗 ",
                 #    txt_maximized="🗖 ",
                 #    txt_minimized="🗕 ",
-                #),
+                # ),
                 widget.Chord(
                     chords_colors={
                         "Apps": (f"{colors[2]}", f"{colors[16]}"),
@@ -212,18 +298,16 @@ screens = [
                     name_transform=lambda name: name.upper(),
                 ),
                 widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a - %I:%M %p"),
+                widget.Clock(format="%F %a - %r"),
                 widget.PulseVolume(
                     mute_command="pamixer --toggle-mute",
                     volume_down_command="pamixer --decrease 2",
                     volume_up_command="pamixer --increase 2",
                     volume_app="pamixer",
                 ),
-                widget.Backlight(backlight_name="amdgpu_bl1"),
-                widget.Sep(
-                    linewidth=0,
-                    padding=5
-                ),
+                widget.Backlight(backlight_name=backlight),
+                widget.BatteryIcon() if platform == "laptop" else widget.CPU(),
+                widget.Sep(linewidth=0, padding=5),
             ],
             bar_thickness,
             background=f"{colors[0]}",
@@ -234,8 +318,15 @@ screens = [
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+    ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
@@ -251,28 +342,28 @@ floating_layout = layout.Floating(
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),   # gitk
-        Match(wm_class="dialog"),         # dialog boxes
-        Match(wm_class="download"),       # downloads
-        Match(wm_class="error"),          # error msgs
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="dialog"),  # dialog boxes
+        Match(wm_class="download"),  # downloads
+        Match(wm_class="error"),  # error msgs
         Match(wm_class="file_progress"),  # file progress boxes
-        Match(wm_class='kdenlive'),       # kdenlive
-        Match(wm_class="makebranch"),     # gitk
-        Match(wm_class="maketag"),        # gitk
-        Match(wm_class="notification"),   # notifications
-        Match(wm_class='pinentry-gtk-2'), # GPG key password entry
-        Match(wm_class="ssh-askpass"),    # ssh-askpass
-        Match(wm_class="toolbar"),        # toolbars
-        Match(wm_class="Yad"),            # yad boxes
-        Match(title="branchdialog"),      # gitk
-        Match(title='Confirmation'),      # tastyworks exit box
-        Match(title='Qalculate!'),        # qalculate-gtk
-        Match(title="pinentry"),          # GPG key password entry
-        Match(title="tastycharts"),       # tastytrade pop-out charts
-        Match(title="tastytrade"),        # tastytrade pop-out side gutter
-        Match(title="tastytrade - Portfolio Report"), # tastytrade pop-out allocation
-        Match(wm_class="tasty.javafx.launcher.LauncherFxApp"), # tastytrade settings
-    ]
+        Match(wm_class="kdenlive"),  # kdenlive
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="notification"),  # notifications
+        Match(wm_class="pinentry-gtk-2"),  # GPG key password entry
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(wm_class="toolbar"),  # toolbars
+        Match(wm_class="Yad"),  # yad boxes
+        Match(title="branchdialog"),  # gitk
+        Match(title="Confirmation"),  # tastyworks exit box
+        Match(title="Qalculate!"),  # qalculate-gtk
+        Match(title="pinentry"),  # GPG key password entry
+        Match(title="tastycharts"),  # tastytrade pop-out charts
+        Match(title="tastytrade"),  # tastytrade pop-out side gutter
+        Match(title="tastytrade - Portfolio Report"),  # tastytrade pop-out allocation
+        Match(wm_class="tasty.javafx.launcher.LauncherFxApp"),  # tastytrade settings
+    ],
 )
 auto_fullscreen = True
 focus_on_window_activation = "smart"
@@ -285,26 +376,27 @@ auto_minimize = True
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
 
+
 @hook.subscribe.startup_once
 def autostart():
     subprocess.Popen([config + "autostart.sh"])
+
 
 @hook.subscribe.startup
 def start_always():
     subprocess.Popen(["xsetroot", "-cursor_name", "left_ptr"])
 
+
 @hook.subscribe.client_new
 def float_steam(window):
     wm_class = window.window.get_wm_class()
     w_name = window.window.get_name()
-    if (
-        wm_class == ("Steam", "Steam")
-        and (
-            w_name != "Steam"
-            or "PMaxSize" in window.window.get_wm_normal_hints().get("flags",())
-        )
+    if wm_class == ("Steam", "Steam") and (
+        w_name != "Steam"
+        or "PMaxSize" in window.window.get_wm_normal_hints().get("flags", ())
     ):
         window.floating = True
+
 
 @hook.subscribe.client_new
 def float_firefox(window):
@@ -312,5 +404,6 @@ def float_firefox(window):
     w_name = window.window.get_name()
     if wm_class == ("Places", "Firefox") and w_name == "Library":
         window.floating = True
+
 
 wmname = "LG3D"
