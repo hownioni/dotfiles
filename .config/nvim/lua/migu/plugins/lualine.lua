2b717baa34710760fb77a8c942df6e9f4c328e05
Honi <honihoneyhowni@gmail.com>
2024-11-18 23:32:52 -0600

    Added blankline to nvim config

--- .config/nvim/lua/migu/plugins/lualine.lua
+++ .config/nvim/lua/migu/plugins/lualine.lua
@@ -1,12 +1,24 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "VeryLazy",
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "pywal",
-            },
-        })
-    end,
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		event = "VeryLazy",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "pywal",
+				},
+			})
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+		config = function()
+			require("ibl").setup()
+		end,
+	},
 }
